c72df10240c06a443b5ab599545d16c82dac9ddb
youhyeoneee <khukyh2018@khu.ac.kr>
2024-09-20 23:01:48 +0900

    feat: 상품 목록 조회 인증 허용

--- src/main/java/com/yhkim/yousinsa/domain/auth/config/WebSecurityConfig.java
+++ src/main/java/com/yhkim/yousinsa/domain/auth/config/WebSecurityConfig.java
@@ -56,7 +56,8 @@ public class WebSecurityConfig {
         http
                 .csrf((AbstractHttpConfigurer::disable))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/users/signup", "/api/users/login", "/api/auth/token", "/h2-console/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/api/users/signup", "/api/users/login", "/api/auth/token", "/api/products",
+                                "/h2-console/**", "/swagger-ui/**").permitAll()
                         
                         .anyRequest().authenticated())
                 .exceptionHandling(
