37d3a164b9a8a7ce49c5165d9539b29f0f2622b7
youhyeoneee <khukyh2018@khu.ac.kr>
2024-09-20 20:24:17 +0900

    feat: jwt & security 관련 클래스 및 config 추가

---
+++ src/main/java/com/yhkim/yousinsa/global/auth/UserDetailsImpl.java
@@ -0,0 +1,24 @@
+package com.yhkim.yousinsa.global.auth;
+
+import com.yhkim.yousinsa.domain.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public record UserDetailsImpl(User user) implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+    
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
---
+++ src/main/java/com/yhkim/yousinsa/global/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.yhkim.yousinsa.global.auth.service;
+
+import com.yhkim.yousinsa.domain.user.entity.User;
+import com.yhkim.yousinsa.domain.user.repository.UserRepository;
+import com.yhkim.yousinsa.global.auth.UserDetailsImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    
+    private final UserRepository userRepository;
+    
+    // TODO: UsernameNotFoundException GlobalExceptionHandler 추가
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + "not found."));
+        return new UserDetailsImpl(user);
+    }
+}
