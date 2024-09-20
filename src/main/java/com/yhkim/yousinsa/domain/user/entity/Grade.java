d97b23813012c7f8a02657a36fca86746d12d161
youhyeoneee <khukyh2018@khu.ac.kr>
2024-09-20 23:27:01 +0900

    feat: 등급에 할인율 추가

--- src/main/java/com/yhkim/yousinsa/domain/user/entity/Grade.java
+++ src/main/java/com/yhkim/yousinsa/domain/user/entity/Grade.java
@@ -8,16 +8,17 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Grade {
     
-    NONE(0, "등급없음"),
-    NEWBE(1, "뉴비"),
-    ROOKIE(2, "루키"),
-    MEMBER(3, "멤버"),
-    BRONZE(4, "브론즈"),
-    SILVER(5, "실버"),
-    GOLD(6, "골드"),
-    PLATINUM(7, "플래티넘"),
-    DIAMOND(8, "다이아몬드");
+    NONE(0, "등급없음", 0),
+    NEWBE(1, "뉴비", 2),
+    ROOKIE(2, "루키", 4),
+    MEMBER(3, "멤버", 5),
+    BRONZE(4, "브론즈", 6),
+    SILVER(5, "실버", 7),
+    GOLD(6, "골드", 8),
+    PLATINUM(7, "플래티넘", 9),
+    DIAMOND(8, "다이아몬드", 10);
     
     private final Integer level;
     private final String name;
+    private final Integer discountRate;
 }
